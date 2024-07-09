d445ca329c181d75fec0460e7cb22a7bfa8ccbd1
Antonella Domenez <domenezantonellaa@gmail.com>
2024-07-09 10:02:29 +1000

    new page projects

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,6 @@ import { useTranslation } from "react-i18next";
 import LanguageSelector from "./LanguageSelector";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/menu.css";
-import logo from "../assets/logo.svg";
 import "animate.css"; // Importar animate.css
 
 const MyNavbar = () => {
@@ -20,13 +19,11 @@ const MyNavbar = () => {
       <Navbar collapseOnSelect expand="lg" className="navbar-custom">
         <Container>
           <Navbar.Brand href="/">
-            <img
-              src={logo}
-              width="100"
-              height="100"
-              className="d-inline-block align-top"
-              alt="Logo"
-            />
+            <div>
+              <h2 className="logo-title" style={{ fontFamily: "TAN Pearl" }}>
+                Fancy IT
+              </h2>
+            </div>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
@@ -34,6 +31,7 @@ const MyNavbar = () => {
               <Nav.Link href="/">{t("home")}</Nav.Link>
               <Nav.Link href="/about-us">{t("about_us")}</Nav.Link>
               <Nav.Link href="/services">{t("services")}</Nav.Link>
+              <Nav.Link href="/projects">{t("projects")}</Nav.Link>
               <Button href="/contact" className="custom-button text-center">
                 {t("contact_us")}
               </Button>
