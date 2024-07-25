97a2763b19f0b490114e1e104074ff864e02bd2e
Antonella Domenez <86069460+antonelladomenez@users.noreply.github.com>
2024-07-25 12:29:35 +1000

    Nombre de la rama (#7)
    * new page projects (#3)
    
    * Initial commit
    
    * new page projects
    
    * resolviendo branches
    
    * nuevos cambios
    
    * Añadir nuevo proyecto y refactorizar Projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,23 +3,38 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 import MyNavbar from "../components/Menu";
 import "../styles/Services.css";
 import Footer from "../components/Footer";
-import project2 from "../images/fisioTeamLogo.png";
-import project1 from "../images/Just-Devine-Gelato-Solid-Colour-Logo.png";
 import TextAnimation from "../components/textAnimation";
 import { useTranslation } from "react-i18next";
 import { useInView } from "react-intersection-observer";
 import "animate.css";
-import { Link } from "react-router-dom";
+import ProjectCard from "../components/ProjectCard";
+import project1 from "../images/Just-Devine-Gelato-Solid-Colour-Logo.png";
+import project2 from "../images/fisioTeamLogo.png";
+import project3 from "../images/factoryNails.png";
 
 const Projects = () => {
   const { t } = useTranslation();
   const { ref: titleRef, inView: titleInView } = useInView({
     triggerOnce: true,
   });
-  const { ref: imgRef, inView: imgInView } = useInView({ triggerOnce: true });
-  const { ref: projectFisio, inView: projectFisioImgInView } = useInView({
-    triggerOnce: true,
-  });
+
+  const projects = [
+    {
+      link: "https://justdevinegelato.com.au/",
+      image: project1,
+      alt: "Just Devine",
+    },
+    {
+      link: "https://www.fisioteam.kesug.com/",
+      image: project2,
+      alt: "FisioTeam",
+    },
+    {
+      link: "https://www.factorynails.wuaze.com/",
+      image: project3,
+      alt: "Factory Nails",
+    },
+  ];
 
   return (
     <>
@@ -36,50 +51,16 @@ const Projects = () => {
           </Row>
         </div>
         <TextAnimation />
-
-        <Row>
+        <Row className="my-sm-4 my-3">
           <Col
             xs={12}
-            className="my-4 service-section justify-content-center align-items-center"
+            className="my-sm-5 my-3 service-section justify-content-center align-items-center"
           >
             <Card>
-              <Row className="no-gutters">
-                <Col
-                  md={6}
-                  ref={imgRef}
-                  className={imgInView ? "animate__slideInFromCenterLeft " : ""}
-                >
-                  {" "}
-                  <Link to="https://justdevinegelato.com.au/" target="_blank">
-                    {" "}
-                    <Card.Img
-                      src={project1}
-                      alt="Just Devine"
-                      style={{
-                        marginBottom: "5px",
-                        height: "200px",
-                        width: "200px",
-                      }}
-                    />
-                  </Link>
-                </Col>
-                <Col
-                  md={6}
-                  ref={projectFisio}
-                  className={
-                    projectFisioImgInView
-                      ? "animate__slideInFromCenterLeft"
-                      : ""
-                  }
-                >
-                  <Link to="https://www.fisioteam.kesug.com/" target="_blank">
-                    <Card.Img
-                      src={project2}
-                      alt="FisioTeam"
-                      style={{ height: "200px", width: "200px" }}
-                    />
-                  </Link>
-                </Col>
+              <Row className="no-gutters text-center">
+                {projects.map((project, index) => (
+                  <ProjectCard key={index} project={project} />
+                ))}
               </Row>
             </Card>
           </Col>
